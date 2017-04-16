5d444e9cf17ea1ef679ec3946fc9d2c2c85f7faa
Michal Nawrocki <v25@vault25.net>
2017-04-16 05:24:05 +0200

    Reading dictionary works

--- text2XML/MainWindow.xaml.cs
+++ text2XML/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using text2XML.Models;
 
 namespace text2XML
 {
@@ -23,6 +26,25 @@ namespace text2XML
         public MainWindow()
         {
             InitializeComponent();
+            Lexicon.LoadLexicon("Dictionary\\XML_dictionary.json");
+        }
+
+        private void CommandBinding_Open(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog txtFile = new OpenFileDialog();
+            txtFile.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (txtFile.ShowDialog() == true)
+                txtBoxSource.Text = File.ReadAllText(txtFile.FileName);
+        }
+
+        private void CommandBinding_Save(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void CommandBinding_Exit(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
